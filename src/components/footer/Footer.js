1b0920cbb01ba5be26a484023d3e275058fa6d28
AbhishekR14 <aranjan0288@gmail.com>
2026-05-18 23:42:21 +0530

    Build fix

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,13 +1,11 @@
 import React, {useContext} from "react";
 import "./Footer.scss";
 import {Fade} from "react-reveal";
-import StyleContext from "../../contexts/StyleContext";
 
 export default function Footer() {
   return (
     <Fade bottom duration={1000} distance="5px">
-      <div className="footer-div">
-        
+      <div className="footer-div">        
       </div>
     </Fade>
   );
